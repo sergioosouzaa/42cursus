59e8e47c4db02389580361183ac85d6e4a6c997a
Sergio Souza <sergiosouzauff@gmail.com>
2022-06-06 08:41:58 -0300

    push done

--- ft_split.c
+++ ft_split.c
@@ -1,5 +1,5 @@
 #include<stdlib.h>
-
+#include<stdio.h>
 static size_t check_malloc_size(char const *s, char c)
 {
     size_t  size;
@@ -20,43 +20,41 @@ static size_t check_malloc_size(char const *s, char c)
             check_div = 1;
         pos++;
     }
+    printf("%ld SIZE \n", size);
     return (size);
 }
 
-static char** malloc_strings(char const *s, char c, char** splited_strings)
+static char* malloc_strings(char const *s, char c, size_t*  pos)
 {
     size_t  size;
-    size_t  pos;
     size_t  check_div;
-    size_t  pos_array;
+    char*   new_string;
 
     size = 0;
-    pos = 0;
     check_div = 1;
-    pos_array = 0;
-    while(*(s + pos))
+    while(*(s + *pos))
     {
-        if(check_div == 1 && *(s + pos) != c)
+        if(check_div == 1 && *(s + *pos) != c)
         {
             check_div = 0;
             size++;
         }
-        else if(*(s + pos) == c)
-        {
-            splited_strings[pos_array] = malloc(size + 1);
-            if(!splited_strings[pos_array])
-                return (0);
-            pos_array++;
-            check_div = 1;
-            size = 0;
-        }
-        pos++;
+        else if(*(s + *pos) == c && check_div == 0)
+            break;
+        (*pos)++;
     }
-    
-    return (splited_strings);
-
+    if(check_div == 0)
+    {
+        new_string = malloc(size + 1);
+        printf("%p POINTER \n", new_string);
+        if(!new_string)
+            return(0);
+        return(new_string);
+    }
+    return(0);
 }
-static char* fill_array(char const *s, char c, char** splited_strings)
+
+static char** fill_array(char const *s, char c, char** splited_strings)
 {
     size_t  pos;
     size_t  pos_array;
@@ -66,23 +64,24 @@ static char* fill_array(char const *s, char c, char** splited_strings)
     pos = 0;
     check_div = 1;
     pos_array = 0;
+    pos_string = 0;
     while(*(s + pos))
     {
         if(check_div == 1 && *(s + pos) != c)
         {
             check_div = 0;
-            splited_strings[pos_array][pos_string] = *(s + pos);
-            pos_string++;
+            splited_strings[pos_array][pos_string++] = *(s + pos);
         }
-        else if(*(s + pos) == c)
+        else if(*(s + pos) == c && check_div == 0)
         {
-            pos_array++;
+            splited_strings[pos_array++][pos_string] = '\0';
             pos_string = 0;
             check_div = 1;
         }
+        else if (*(s + pos) != c )
+            splited_strings[pos_array][pos_string++] = *(s + pos);
         pos++;
     }
-
     return (splited_strings);
 }
 
@@ -91,18 +90,27 @@ char **ft_split(char const *s, char c)
     size_t array_size;
     char** splited_strings;
     size_t  pos;
+    size_t  array_pos;
 
     pos = 0;
+    array_pos = 0;
     array_size = check_malloc_size(s, c) + 1;
     splited_strings = (char**)malloc(array_size);
     if(!splited_strings)
         return(0);
+    while(*(s + pos))
+    {
+        splited_strings[array_pos] = malloc_strings(s, c, &pos);
+        array_pos++;
+    }
+    splited_strings[array_pos] = 0;
     splited_strings = fill_array(s, c, splited_strings);
     return(splited_strings);
 }
 
-#include<stdio.h>
+
 int main(void)
 {
-
+    char** text = ft_split("..AA..gsfer..sadsadasda..", '.');
+    printf("%s \n", text[2]);
 }
